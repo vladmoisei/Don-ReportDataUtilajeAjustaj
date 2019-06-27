8ef4beb1f31e118680d6b9f35d98299f5f2be017
vlad moisei <vladmoisei@yahoo.com>
2019-06-27 10:25:43 +0100

    Add autocomplete data to Elti and Gadda for last values of Cuptor

--- RaportareAjustajV2/wwwroot/js/cuptorElti.js
+++ RaportareAjustajV2/wwwroot/js/cuptorElti.js
@@ -1,15 +1,34 @@
 ﻿// A $( document ).ready() block.
 $(document).ready(function () {
-    console.log("ready!");
+    //console.log("ready!");
     // Inregistrare date cuptor la click submit
     var submitBtn = document.getElementById("btnSubmit");
     submitBtn.addEventListener("click", function () {
-        alert("clickj!");
+        localStorage.setItem("CuptorElti", document.getElementById("Cuptor").value);
+        localStorage.setItem("TratamentTermicElti", document.getElementById("TratamentTermic").value);
+        localStorage.setItem("DataIncarcareElti", document.getElementById("DataIncarcare").value);
+        localStorage.setItem("OraIncarcareElti", document.getElementById("OraIncarcare").value);
+        localStorage.setItem("DataDescarcareElti", document.getElementById("DataDescarcare").value);
+        localStorage.setItem("OraDescarcareElti", document.getElementById("OraDescarcare").value);
+        //alert(localStorage.getItem("DataIncarcare"));
     });
-    // Actualizare date in functie de modificare input cuptor
+
+    // Actualizare date in functie de modificare inputfield cuptor
     var cuptorElement = document.getElementById("Cuptor");    
     cuptorElement.addEventListener("change", function () {
-        var cuptorElementText = document.getElementById("Cuptor").value;
-        console.log(cuptorElementText);
+        var cuptorElementValue = document.getElementById("Cuptor").value;
+        if (cuptorElementValue === localStorage.getItem("CuptorElti")) {
+            var TratamentTermic = document.getElementById("TratamentTermic");
+            var DataIncarcare = document.getElementById("DataIncarcare");
+            var OraIncarcare = document.getElementById("OraIncarcare");
+            var DataDescarcare = document.getElementById("DataDescarcare");
+            var OraDescarcare = document.getElementById("OraDescarcare");
+            TratamentTermic.value = localStorage.getItem("TratamentTermicElti");
+            DataIncarcare.value = localStorage.getItem("DataIncarcareElti");
+            OraIncarcare.value = localStorage.getItem("OraIncarcareElti");
+            DataDescarcare.value = localStorage.getItem("DataDescarcareElti");
+            OraDescarcare.value = localStorage.getItem("OraDescarcareElti");
+        }
+        //console.log(cuptorElementValue);
     });
 });
---
+++ RaportareAjustajV2/wwwroot/js/cuptorGadda.js
@@ -0,0 +1,34 @@
+﻿// A $( document ).ready() block.
+$(document).ready(function () {
+    //console.log("ready!");
+    // Inregistrare date cuptor la click submit
+    var submitBtn = document.getElementById("btnSubmit");
+    submitBtn.addEventListener("click", function () {
+        localStorage.setItem("CuptorGadda", document.getElementById("Cuptor").value);
+        localStorage.setItem("TratamentTermicGadda", document.getElementById("TratamentTermic").value);
+        localStorage.setItem("DataIncarcareGadda", document.getElementById("DataIncarcare").value);
+        localStorage.setItem("OraIncarcareGadda", document.getElementById("OraIncarcare").value);
+        localStorage.setItem("DataDescarcareGadda", document.getElementById("DataDescarcare").value);
+        localStorage.setItem("OraDescarcareGadda", document.getElementById("OraDescarcare").value);
+        //alert(localStorage.getItem("DataIncarcare"));
+    });
+
+    // Actualizare date in functie de modificare inputfield cuptor
+    var cuptorElement = document.getElementById("Cuptor");
+    cuptorElement.addEventListener("change", function () {
+        var cuptorElementValue = document.getElementById("Cuptor").value;
+        if (cuptorElementValue === localStorage.getItem("CuptorGadda")) {
+            var TratamentTermic = document.getElementById("TratamentTermic");
+            var DataIncarcare = document.getElementById("DataIncarcare");
+            var OraIncarcare = document.getElementById("OraIncarcare");
+            var DataDescarcare = document.getElementById("DataDescarcare");
+            var OraDescarcare = document.getElementById("OraDescarcare");
+            TratamentTermic.value = localStorage.getItem("TratamentTermicGadda");
+            DataIncarcare.value = localStorage.getItem("DataIncarcareGadda");
+            OraIncarcare.value = localStorage.getItem("OraIncarcareGadda");
+            DataDescarcare.value = localStorage.getItem("DataDescarcareGadda");
+            OraDescarcare.value = localStorage.getItem("OraDescarcareGadda");
+        }
+        //console.log(cuptorElementValue);
+    });
+});
